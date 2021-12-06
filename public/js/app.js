1b64e46339410d973bbdce28405c29193b82c368
mostafa <mostafamnbvcxz5@gmail.com>
2021-12-06 13:29:39 +0330

    make PM and AM recognization and Day and Date

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,13 @@
-const DigitsText = document.querySelector("h2");
-const DayNight = document.querySelector("i");
+const DigitsText = document.querySelector('h2');
+const DayNight = document.querySelector('i');
+const Dtext = document.getElementById('DayText');
+const DateText = document.getElementById('DateText');
 const Sline = document.getElementById('Sline');
 const Mline = document.getElementById('Mline');
 const Hline = document.getElementById('Hline');
-
 const date = new Date;
 const Hdate =  date.getHours();
+// if it day or night
 if(Hdate >=6 && Hdate<= 18){
 
     DayNight.classList.remove("fa-moon");
@@ -15,21 +17,24 @@ else{
     DayNight.classList.remove("fa-sun");
     DayNight.classList.add("fa-moon");
 }
-
-
-
+// the digits clock
 function GenerateClock(){
     const date = new Date;
     const text = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    DigitsText.innerHTML = `${text} PM`;
+    if(date.getHours>= 0&&date.getHours<12){
+        DigitsText.innerHTML = `${text} AM`;
+    }
+    else{
+        DigitsText.innerHTML = `${text} PM`;
+    }
+    
 }
 GenerateClock();
-
 setInterval(()=>{
     GenerateClock();
 },1000)
 
-
+// the analog clock
 function clock(){
     const date = new Date;
     let Sec = date.getSeconds()/60 *360 + 90;
@@ -42,4 +47,29 @@ function clock(){
 }
 clock();
 setInterval(clock,1000)
+// showing day 
+const Day = date.getDay();
+if(Day == 1){
+    Dtext.innerHTML= 'its Moday';
+}
+else if(Day == 2){
+    Dtext.innerHTML= 'its Tuesday';
+}
+else if(Day == 3){
+    Dtext.innerHTML= 'its Wednesday';
+}
+else if(Day == 4){
+    Dtext.innerHTML= 'its Thursday';
+}
+else if(Day == 4){
+    Dtext.innerHTML= 'its Friday';
+}
+else if(Day == 4){
+    Dtext.innerHTML= 'its Saturday';
+}
 
+// showing date 
+const toYear = date.getFullYear();
+const toMonth = date.getMonth();
+const toDay = date.getDay();
+DateText.innerHTML = `${toYear}/${toMonth}/${toDay}`
